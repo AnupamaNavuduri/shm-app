c3c4d31a1657ab5f20ea015497273ff06ed2ebd7
Anupama Navuduri <63915107+AnupamaNavuduri@users.noreply.github.com>
2025-07-07 13:18:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('🎈 SHM App')
+st.title('SHM App')
 
-st.write('This app serves as a dashboard to see if given sensor value is an anomaly or not')
+st.info('This app serves as a dashboard to see if given sensor value is an anomaly or not')
